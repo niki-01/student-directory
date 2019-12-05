519eb8a7ee563b277056bba657d16fd390c54e7d
Niki <niki.manoledaki@gmail.com>
2019-12-05 18:13:38 +0000

    Adds interactive menu

--- directory.rb
+++ directory.rb
@@ -2,8 +2,8 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   
-  students = []
-  name = gets.split
+  @students = []
+  name = gets.chomp
   
   cohort_months = {
     "" => "N/A", "January" => :January, "February" => :February, "March" => :March, 
@@ -14,24 +14,23 @@ def input_students
   
   while !name.empty? do
     puts "Please enter the student's cohort"
-    cohort = cohort_months[gets.capitalize.split]
+    cohort = cohort_months[gets.capitalize.chomp]
+    
     while cohort == nil do
       puts "An error occured \nPlease enter the student's cohort"
-      cohort = cohort_months[gets.capitalize.split]
+      cohort = cohort_months[gets.capitalize.chomp]
     end
 
-    students << {name: name, cohort: cohort}
-    if students.count == 1
+    @students << {name: name, cohort: cohort}
+    if @students.count == 1
       puts "Now we have 1 student" 
     else 
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     puts "Please enter another name, or hit return to exit"
     
-    name = gets.split
+    name = gets.chomp
   end
-  
-  students
 end
 
 def print(students)
@@ -42,12 +41,15 @@ count = 0
   end
 end
 
-def print_by_cohort(students)
-  puts "Which cohort would you like?"
-  input = gets.split.capitalize
+def header
   puts "The students of Villains Academy".center(50)
   puts "-------------".center(53)
-  
+end
+
+def print_by_cohort(students)
+  puts "Which cohort would you like?"
+  input = gets.capitalize.chomp
+  header
   students.each do |student|
     if student[:cohort] == input.to_sym
       puts student[:name].center(50)
@@ -56,11 +58,40 @@ def print_by_cohort(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great student(s)".center(51)
+  puts "Overall, we have #{@students.count} great student(s)".center(51)
 end
 
-students = input_students
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-# print(students)
-print_by_cohort(students)
-print_footer(students)
+def case_selection(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you mean, try again"
+  end
+end
+
+def show_students
+  print_by_cohort(@students)
+  print_footer(@students)
+end
+
+def interactive_menu
+  @students = []
+  loop do
+    print_menu
+    selection = gets.chomp
+    case_selection(selection)
+  end
+end
+
+interactive_menu
