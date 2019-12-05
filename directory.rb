7b1adf9c82245302f3dd8169a33d4b8b5348c4f0
Niki <niki.manoledaki@gmail.com>
2019-12-05 16:45:23 +0000

    Prints by cohort

--- directory.rb
+++ directory.rb
@@ -13,13 +13,6 @@ def input_students
 
   
   while !name.empty? do
-    puts "Please enter the student's favourite hobby"
-    hobby = gets.chomp
-    hobby = "N/A" if hobby.empty? == true
-    puts "Please enter the student's country of birth"
-    country = gets.chomp
-    country = "N/A" if country.empty? == true
-    
     puts "Please enter the student's cohort"
     cohort = cohort_months[gets.capitalize.chomp]
     while cohort == nil do
@@ -27,8 +20,12 @@ def input_students
       cohort = cohort_months[gets.capitalize.chomp]
     end
 
-    students << {name: name, hobby: hobby, country: country, cohort: cohort}
-    puts "Now we have #{students.count} student(s)"
+    students << {name: name, cohort: cohort}
+    if students.count == 1
+      puts "Now we have 1 student" 
+    else 
+      puts "Now we have #{students.count} students"
+    end
     puts "Please enter another name, or hit return to exit"
     
     name = gets.chomp
@@ -37,16 +34,24 @@ def input_students
   students
 end
 
-def print_header
-  puts "The students of Villains Academy".center(50)
-  puts "-------------".center(53)
-end
-
 def print(students)
 count = 0
   until count >= students.length
-    puts "#{students[count][:name]} (Hobby: #{students[count][:hobby]}) (Country: #{students[count][:country]}) (Cohort: #{students[count][:cohort]})"
+    puts "#{students[count][:name]} (Cohort: #{students[count][:cohort]})"
     count += 1
+  end
+end
+
+def print_by_cohort(students)
+  puts "Which cohort would you like?"
+  input = gets.chomp.capitalize
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(53)
+  
+  students.each do |student|
+    if student[:cohort] == input.to_sym
+      puts student[:name].center(50)
+    end
   end
 end
 
@@ -56,6 +61,6 @@ end
 
 students = input_students
 
-print_header
-print(students)
+# print(students)
+print_by_cohort(students)
 print_footer(students)
