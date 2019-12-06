a763047a1b49913e94c5bb288973ae0ef0ebdd4e
Niki <niki.manoledaki@gmail.com>
2019-12-06 14:39:23 +0000

    Load student list data from CSV file

--- directory.rb
+++ directory.rb
@@ -32,10 +32,21 @@ def save_students
   # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
+    csv_line = student_data.join(',')
     file.puts csv_line
   end
   file.close
+  puts "Data saved successfully."
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line| 
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+  puts "Data loaded successfully."
 end
 
 def interactive_menu
@@ -49,6 +60,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -65,6 +77,8 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
